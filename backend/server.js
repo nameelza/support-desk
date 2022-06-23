2cf80cfda34f043f63504bab37736374cb828ca0
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-23 15:30:58 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv");
+const PORT = 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
