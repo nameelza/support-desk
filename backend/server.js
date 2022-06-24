717654519cf84d34a100410245daaf7400033b7c
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-24 14:02:09 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get((req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the Support Desk API" });
 });
 
