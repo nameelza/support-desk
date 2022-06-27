46481f228e37876bb1268f08fd7de469cd515d73
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-27 15:53:02 -0700

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,6 +49,10 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = res.body;
+
+  const user = await User.findOne({ email });
+
   res.send("Login Route");
 });
 
