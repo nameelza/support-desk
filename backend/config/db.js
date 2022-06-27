26ed0156297ac96b14b1ecbf96f147e9d5c65e35
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-27 14:59:51 -0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,5 +6,8 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
   }
 };
+
+module.exports = connectDB;
