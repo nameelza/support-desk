6c203bed441d34741bc568cfe808ca7e4df3e4e6
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-27 16:16:59 -0700

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -16,6 +16,19 @@ const protect = asyncHandler(async (req, res, next) => {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       // Get user from token
       req.user = await User.findById(decoded.id).select("-password");
-    } catch (error) {}
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized");
   }
 });
+
+module.exports = { protect };
