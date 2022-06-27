5a6b576e2b4dc2abc8a78d7f027cc3f2c18f4e52
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-27 14:36:30 -0700

    Create errorMiddleware.js

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,9 @@
+const errorHandler = (err, req, res, next) => {
+  res.status(res.statusCode ?? 500);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+module.exports = { errorHandler };
