e5b776bceade61318ff828501b7204f70f67a3b6
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-27 14:36:33 -0700

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,8 @@ const registerUser = (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    res.status(400).json({ message: "Please fill in all fields" });
+    res.status(400);
+    throw new Error("Please include all fields");
   }
 
   res.send("Register Route");
