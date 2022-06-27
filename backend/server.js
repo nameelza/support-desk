e6b9cf9310c8484fd75a34f83f41d7b331bc462a
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-27 14:56:04 -0700

    express

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
