8afe7ade8eb50cbbe83f17201b26307bf544ab6c
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-27 14:17:40 -0700

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { registerUser, loginUser } = require("../controllers/userController");
 
 router.post("/", (req, res) => {
   res.send("Register Route");
