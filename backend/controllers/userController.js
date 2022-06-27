ca0079702c3a2c6fed7823cb6c23882019ad3f74
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-27 14:23:05 -0700

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,13 @@
+// @desc    Register a new user
+// @route   /api/users
+// @access  Public
 const registerUser = (req, res) => {
   res.send("Register Route");
 };
 
+// @desc    Login a user
+// @route   /api/users/login
+// @access  Public
 const loginUser = (req, res) => {
   res.send("Login Route");
 };
