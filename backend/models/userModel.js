e3aa8cf84d1f3456be053b8ae56c7f139127f32a
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-27 15:14:19 -0700

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,17 +1,29 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Please add an email"],
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
