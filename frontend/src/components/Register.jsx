69c3370aaa5928de0c701c6ee0d6e16ed45f1e2b
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-28 11:22:27 -0700

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,8 +1,75 @@
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+
 function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const { name, email, password, passwordConfirm } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+      <section>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section>
+        <form onSubmit={onSubmits}>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            placeholder="Enter your name"
+            onChange={onChange}
+            required
+          />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={onChange}
+            required
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Enter password"
+            onChange={onChange}
+            required
+          />
+          <input
+            type="password"
+            id="passwordConfirm"
+            name="passwordConfirm"
+            value={passwordConfirm}
+            placeholder="Confirm password"
+            onChange={onChange}
+            required
+          />
+          <div>
+            <button className="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 }
 
