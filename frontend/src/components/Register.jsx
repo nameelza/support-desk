4cefc115e1cc82dfc0c4e005b5e29751f5315c6f
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-28 14:21:49 -0700

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
+import { register } from "../features/auth/authSlice";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -12,6 +13,12 @@ function Register() {
   });
 
   const { name, email, password, passwordConfirm } = formData;
+
+  const dispatch = useDispatch();
+
+  const { user, isError, isSuccess, isLoading, message } = useSelector(
+    (state) => state.auth
+  );
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -25,6 +32,14 @@ function Register() {
 
     if (password !== passwordConfirm) {
       return toast.error("Passwords do not match");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      dispatch(register(userData));
     }
   };
 
