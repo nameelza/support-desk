1913fec0b9f302838c77111b458439dc8e5e132c
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-28 14:21:44 -0700

    Update authSlice.js

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -8,12 +8,18 @@ const initialState = {
   message: "",
 };
 
+// Register new user
 export const register = createAsyncThunk(
   "auth/register",
   async (user, thunkAPI) => {
-    console.log(user)
+    console.log(user);
   }
 );
+
+// Login existing user
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  console.log(user);
+});
 
 export const authSlice = createSlice({
   name: "auth",
