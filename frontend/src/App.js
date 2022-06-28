39687acb145d0b1dc69240407b129e6c196091d1
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-28 10:58:49 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
 function App() {
-  return <div>My App</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
