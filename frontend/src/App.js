b5e3b5d8328c070715ea2d88c6fdb597e064d01c
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-30 14:21:09 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NewTicket from "./pages/NewTicket";
 import Register from "./pages/Register";
-import Header from "./components/Header";
-import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
