46c2060a9f1595ab9a94926d5614a7e012ebfb34
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-30 14:59:49 -0700

    Update store.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-import ticketReducer from "../features/ticket/ticketSlice";
+import ticketReducer from "../features/tickets/ticketSlice";
 
 export const store = configureStore({
   reducer: {
