3161120f500adcbe94df61676c73f541634ee60f
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-30 12:07:59 -0700

    Update ticketController.js

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -75,6 +75,35 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
+// @desc    Delete ticket
+// @route   DELETE /api/tickets/:id
+// @access  Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  // Get user id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+
+  await ticket.remove();
+
+  res.status(200).json({ success: true });
+});
+
 module.exports = {
   getTickets,
   getTicket,
