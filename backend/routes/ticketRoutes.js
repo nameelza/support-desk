fb4120ba112686b7833930ba80a5b43315b663d3
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-30 12:04:17 -0700

    Update ticketRoutes.js

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getTickets, createTicket } = require("../controllers/ticketController");
+const {
+  getTickets,
+  getTicket,
+  createTicket,
+} = require("../controllers/ticketController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+router.route("/:id").get(protect, getTicket);
 
 module.exports = router;
