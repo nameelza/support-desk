9dd4c6d829d3057d0c36f15b75ab674c45c71a4b
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-30 11:19:30 -0700

    Update ticketModel.js

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ticketSchema = mongoose.Schema(
   {
     user: {
-      type: mongoose.Schema.Types.objectId,
+      type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: "User",
     },
