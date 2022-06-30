d294640d99161b8519d22f5aa07e63cf99f9dfe9
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-30 12:32:44 -0700

    Create useAuthStatus.js

---
+++ frontend/src/hooks/useAuthStatus.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (user) {
+      setLoggedIn(true);
+    }
+    setLoading(false);
+  }, [user]);
+
+  return { loggedIn, isLoading };
+};
