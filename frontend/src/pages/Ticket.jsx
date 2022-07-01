e989daa50c8949df01354ed319a3cbc773e43803
nameelza <82921161+nameelza@users.noreply.github.com>
2022-07-01 09:45:55 -0700

    Update Ticket.jsx

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,5 +1,10 @@
+import { useSelector, useDispatch } from "react-redux";
+import { getTicket, reset } from "../features/tickets/ticketService";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+
 function Ticket() {
-  return <h1>tickdfet</h1>;
+  return <h1>ticket</h1>;
 }
 
 export default Ticket;
