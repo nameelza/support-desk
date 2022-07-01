21266c3c2df5d158ef12212f29a77671ac9744a1
nameelza <82921161+nameelza@users.noreply.github.com>
2022-07-01 10:01:21 -0700

    Update Ticket.jsx

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getTicket, reset } from "../features/tickets/ticketService";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { getTicket, reset } from "../features/tickets/ticketSlice";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 
 function Ticket() {
+  const dispatch = useDispatch();
+  const { ticketId } = useParams();
+
+  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.tickets
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    dispatch(getTicket(ticketId));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isError, ticketId, message]);
+
   return <h1>ticket</h1>;
 }
 
