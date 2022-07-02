59ae3c3780b17e472788ccc48b4f2f6dafc5439e
nameelza <82921161+nameelza@users.noreply.github.com>
2022-07-01 17:24:08 -0700

    addNote

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -28,4 +28,33 @@ const getNotes = asyncHandler(async (req, res) => {
   res.status(200).json(notes);
 });
 
-module.exports = { getNotes };
+// @desc    Create ticket note
+// @route   POST /api/tickets/:ticketId/notes
+// @access  Private
+const addNote = asyncHandler(async (req, res) => {
+  // Get user id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId);
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  const note = await Note.create({
+    text: req.body.text,
+    isStaff: false,
+    ticket: req.params.ticketId,
+    user: req.user.id,
+  });
+
+  res.status(200).json(note);
+});
+
+module.exports = { getNotes, addNote };
