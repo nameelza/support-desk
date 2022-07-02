43d262b09f87568277374f129bf9c6eb695aed64
nameelza <82921161+nameelza@users.noreply.github.com>
2022-07-01 17:20:45 -0700

    Update noteRoutes.js

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { getNotes } = require("../controlles/noteController");
+const { getNotes } = require("../controllers/noteController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getNotes);
