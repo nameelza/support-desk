dee64b8f35d35ac11c0d957a9a7d4e8321050082
nameelza <82921161+nameelza@users.noreply.github.com>
2022-07-14 16:56:53 -0700

    env

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://nameelza:SS3siy6DV7c9yuA@cluster0.9kpyoba.mongodb.net/supportdeskdb?retryWrites=true&w=majority"
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(`Error: ${error.message}`.red.underline.bold);
